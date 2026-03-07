18666eab9602fbf0380a4fd13e9a72ce72b565d2
dnljnt <designcodeninja@gmail.com>
2026-03-07 11:25:35 +0800

    feat: add HeroSection, MainFooter, and MainNavbar components
    - Created HeroSection.vue for the landing page with a background image and call-to-action buttons.
    - Added MainFooter.vue containing navigation links and copyright information.
    - Implemented MainNavbar.vue with logo, navigation links, search bar, and action buttons for a responsive design.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup>
+import HeroSection from './components/HeroSection.vue';
+import MainFooter from './components/MainFooter.vue';
+import MainNavbar from './components/MainNavbar.vue';
+
 </script>
 
 <template>
-  <main class="min-h-screen flex items-center justify-center bg-gray-100">
-    <div class="text-center p-8 bg-white rounded-xl shadow-lg">
-      <p class="text-gray-600">
-	      Hello World
-        Setup Vue.js dan Tailwind CSS v4 berhasil!
-      </p>
-      <br>
-      <button type="button" class="bg-blue-500 text-blue-100 text-xl font-bold p-3 rounded-xl cursor-pointer">Click me!</button>
-    </div>
-  </main>
+  <MainNavbar />
+  <HeroSection />
+  <MainFooter />
 </template>
